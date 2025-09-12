9fe360185a12f776f53d2d9002890882cf21f05c
baldus <jjbaldus@gmail.com>
2025-09-11 23:38:53 -0500

    Delete invapp2 directory

--- invapp2/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        "DB_URL",
-        "postgresql+psycopg2://inv:change_me@localhost/invdb"
-    )
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
